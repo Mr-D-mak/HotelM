3357529d1778c9ec954a71809ac207ff9a4c02cf
Mr-D-mak <adeeshmalka@gmail.com>
2025-05-28 17:43:08 +0530

    Initial commit

---
+++ src/app/page/Login/page.js
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+export default function page() {
+
+  return (
+    <div>
+        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+    </div>
+  )
+}
